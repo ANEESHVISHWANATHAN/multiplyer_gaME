19b363539f1130c31f711a1120dd5bb72e242acf
ANEESH <aneesh06042006@gmail.com>
2025-04-17 13:05:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,3 +28,29 @@ app.get('/entry.html', serveFile('entry.html'));
 app.get('/hstdet.html', serveFile('hstdet.html'));
 app.get('/plyrdet.html', serveFile('plyrdet.html'));
 app.get('/lobby.html', serveFile('lobby.html'));
+
+wss.on('connection',(ws)=>{
+ws.on('message',(message)=>{
+  const data = JSON.parse(message);
+  
+  if(data.type === 'createLobby'){
+    const plyrrid = 0;
+    const roomID = generateRoomID();
+    lobbies[roomID]={
+        Host : ws,
+        Players : [{
+                 username : data.username,
+                 icon: data.icon,
+                 id : ws,
+                 plyrid : 0 }]
+    }
+    const lobby = lobbies[roomID].Players[plyrrid].username;
+    console.log("ROOM CREATED WITH ID ${roomID} AND USERNAME ${lobby}"); 
+    ws.send(JSON.stringify({
+      type :'LobbyCreated',
+      roomID : roomID,
+      PlayerID : plyrrid }));
+  }
+  
+  
+    
