8df23d1e86fdd94db0a2a6d522ad71c651f313e3
ANEESH <aneesh06042006@gmail.com>
2025-08-29 16:31:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,11 +75,10 @@ app.post("/makepdf", async (req, res) => {
 
     // Launch Puppeteer
     console.log("[INFO] Launching Puppeteer...");
-    const browser = await 
-    puppeteer.launch({
+    const browser = await puppeteer.launch({
   headless: true,
-  executablePath: '/usr/bin/chromium',
-  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
 });
     console.log("[INFO] Puppeteer launched");
 
